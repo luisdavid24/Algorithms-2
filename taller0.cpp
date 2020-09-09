25f2c36b1e125263e9acb3e691c7b9d35f35286b
unknown <david12.29@hotmail.com>
2020-09-09 11:43:18 -0500

    ya voy bien avanzado con el taller

--- taller0.cpp
+++ taller0.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <cstdlib>
 //Este es el  taller 0 de algoritmos 2
-int radio,lado;
+int vector[12];
+int radio,lado,menor,vecez;
 using namespace std;
 
 void figurasEsfericas(){
@@ -21,14 +22,50 @@ void figurasCuadradas(){
 	cout<<"El valor del area es de : "<<area<<endl;
 	cout<<"El valor del volumen es de "<<volumen<<endl;
 }
+void llenado(int *vector){
+	for(int i=0;i<12;i++)
+	{	cout<<"Valor posicion "<<i<<":";
+		cin>>vector[i];
+	}
+	menor=vector[0];
+}
+void mostrar(int *vector){
+	cout<<"Vector inveso: "<<endl;
+	for(int i=11;i>=0;i--){
+		cout<<"Valor de la posicion "<<i<<": ";
+		cout<<vector[i]<<endl;
+	}
+	for(int i=0;i<12;i++){
+		if(menor>vector[i]){
+			menor=vector[i];
+		}
+	}	
+	
+}
+void busqueda(int *vector){
+	for(int i=0;i<11;i++){
+		if(menor==vector[i]){
+			cout<<"El elemento menor se encuentra en la posicion : "<<i<<endl;
+			vecez++;
+		}
+	}
+}
+void vectorProcedimiento(){
+	llenado(vector);
+	mostrar(vector);
+	cout<<"Este es el menor: "<<menor<<endl;
+	busqueda(vector);
+	cout<<"El numero de vecez que aparece el menor es : "<<vecez;
+}
 int main(){ 
 //	cout<<"Escriba el valor del radio :";
 //	cin>>radio;
 //	figurasEsfericas();
-	cout<<"Escriba el valor del lado :";
-	cin>>lado;
-	figurasCuadradas();
-	
+
+//	cout<<"Escriba el valor del lado :";
+//	cin>>lado;
+//	figurasCuadradas();
+	vectorProcedimiento();	
 
 	return 0;
 }
