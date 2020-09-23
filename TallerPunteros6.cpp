940068cd089acb140337ce34d70b4dfd6512b6b7
unknown <david12.29@hotmail.com>
2020-09-23 13:48:38 -0500

    listo el punto 6

---
+++ TallerPunteros6.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+//Este el punto numero 5 del taller de Punteros C.
+int numero1,numero2;
+
+using namespace std;
+
+void operacion(int&,int&);
+
+int main(){
+	numero1=5;
+	numero2=6;
+	operacion(numero1,numero2);	
+		
+	return 0;
+}
+
+void operacion(int& numero1,int& numero2){
+	cout<<"La suma de los numeros es: "<<numero1+numero2;
+}
